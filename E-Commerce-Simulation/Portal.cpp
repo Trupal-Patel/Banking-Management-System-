4e1f9ccaed1eea47e8161fc73faf94b6eb28dbad
Trupal-Patel <98317107+Trupal-Patel@users.noreply.github.com>
2024-09-12 11:46:26 +0530

    Delete E-Commerce-Simulation directory

--- E-Commerce-Simulation/Portal.cpp
+++
@@ -1,5 +0,0 @@
-#include "Portal.h"
-
-void Portal :: processUserCommand(string command){/*This method will be overridden in the child class.*/}
-
-void Portal :: checkResponse(){/*This method will be overridden in the child class.*/}
